75a5fb66e8e3e8327df1bfeaa4fc952164fe849d
jeff oriente <jdanieloriente@gmail.com>
2018-09-27 16:06:19 -0400

    Almost Finished Cats

--- cats/index.js
+++ cats/index.js
@@ -1,6 +1,9 @@
 function handleCatThumbnail(){
 $('.thumbnails').on('click', 'thumbnail', function(event){
 const imageSource = $(this).find('img').attr('src');
+const altSource = $(this).find('img').attr('alt');
+$('.hero img').attr('src', imageSource);
+$('.hero img').attr('alt', altSource);
 });
 }
 
