3d414a319bbb31f5bfabaeff0b6f2d7fd9d7a7c6
jeff oriente <jdanieloriente@gmail.com>
2018-09-27 12:50:24 -0400

    Added new jQuery code

---
+++ cats/index.js
@@ -0,0 +1,16 @@
+function handleCatThumbnail(){
+$('.thumbnails').on('click', 'thumbnail', function(event){
+const imageSource = $(this).find('img').attr('src');
+});
+}
+
+
+
+
+
+
+
+
+
+
+$(handleCatThumbnail());
